0c6736bba66ff17d3945632000da5ab0d649801e
Krieglerlawoko <krieglerlawokojudge@gmail.com>
2023-07-17 11:57:03 -0700

    new

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -2,11 +2,11 @@
 #define DOG_H
 
 /**
- * strct dog - a dog's basic info
+ * struct dog - a dog's basic info
  * @name: First member
  * @age: Second member
  * @owner: Third member
- * 
+ *
  * Description: Longer description
  */
 struct dog
@@ -16,12 +16,10 @@ struct dog
 	char *owner;
 };
 
-
 /**
  * dog_t - typedef for struct dog
  */
 typedef struct dog dog_t;
-
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
@@ -29,7 +27,6 @@ dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
 char *_strcpy(char *dest, char *src);
 int _strlen(char *s);
-
 
 #endif
 
