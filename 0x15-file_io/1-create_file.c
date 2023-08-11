d426722d00eb8a6203f3442117d617b4a6339527
Krieglerlawoko <krieglerlawokojudge@gmail.com>
2023-08-11 00:08:54 -0700

    new

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -22,7 +22,7 @@ int create_file(const char *filename, char *text_content)
 		for (lenth = 0; text_content[lenth];)
 			lenth++;
 	}
-	fil = open(filename, O_CREATE | O_RDWR | O_TRUNC, 0600);
+	fil = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
 	writ = write(fil, text_content, lenth);
 
 	if (fil == -1 || writ == -1)
