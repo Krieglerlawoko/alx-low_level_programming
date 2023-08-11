7ce14454092fbdf22e596a471daaca6973833f99
Krieglerlawoko <krieglerlawokojudge@gmail.com>
2023-08-10 23:32:42 -0700

    new

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -11,20 +11,21 @@
 
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	char *b;
+	char *buff;
 	ssize_t fd;
 	ssize_t fil;
 	ssize_t rid;
 
 	fil = open(filename, O_RDONLY);
+
 	if (fil == -1)
 		return (0);
 
 	b = malloc(sizeof(char) * letters);
 	rid = read(fd, buf, letters);
-	writ = write(STDOUT_FILENO, b, rid);
+	writ = write(STDOUT_FILENO, buff, rid);
 
-	free(b);
+	free(buff);
 	close(fil);
 
 	return (rid);
