d3aeb2afa0b4402ce1b905085da2333bf5296f3d
Krieglerlawoko <krieglerlawokojudge@gmail.com>
2023-11-30 19:13:41 +0000

    new

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -20,7 +20,7 @@ char *hash_table_get(const hash_table_t *ht, const char *key)
 	if (index >= ht->size)
 		return (NULL);
 
-	node = ht->array[index];
+	n = ht->array[index];
 	while (n && strcmp(n->key, key) != 0)
 		n = n->next;
 
