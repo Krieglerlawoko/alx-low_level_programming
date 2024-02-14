8bb653ede4dcbe8eef4b98f6582e172c86ad1250
Krieglerlawoko <krieglerlawokojudge@gmail.com>
2024-02-14 10:33:31 +0000

    n

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -6,8 +6,11 @@ void first(void) __attribute__ ((constructor));
  * first - Sentence printd before main
  * function is executed
  */
-void first(void)
+void myStartupFun(void) __attribute__((constructor));
+
+void myStartupFun(void)
 {
- printf("You're beat! and yet, you must allow,\n");
- printf("I bore my house upon my back!\n");
+   printf("You're beat! and yet, you must allow,\n");
+   printf("I bore my house upon my back!\n");
 }
+
