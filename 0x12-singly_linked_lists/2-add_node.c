a0f636d1ac1bce8624d46b7df36dceca1f35cbec
Krieglerlawoko <krieglerlawokojudge@gmail.com>
2024-02-14 10:29:18 +0000

    change

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -3,27 +3,26 @@
 #include "lists.h"
 
 /**
- * add_node - this adds a new node at beggining of linked list
- * @head: a double pointer to the list
- * @str: a new string to be added to the node
+ * add_node - adds a new node at the beginning of a linked list
+ * @head: double pointer to the list_t list
+ * @str: new string to add in the node
  *
- * Return: returns the address of the new member, of NULL for fail
+ * Return: the address of the new element, or NULL if it fails
  */
-
 list_t *add_node(list_t **head, const char *str)
 {
+        list_t *new;
 	unsigned int len = 0;
-	list_t *new;
 
 	while (str[len])
-		l++;
+		len++;
 
 	new = malloc(sizeof(list_t));
 	if (!new)
 		return (NULL);
 
-	new->len = len;
 	new->str = strdup(str);
+	new->len = len;
 	new->next = (*head);
 	(*head) = new;
 
--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -11,31 +11,31 @@
  */
 list_t *add_node_end(list_t **head, const char *str)
 {
- list_t *n;
- list_t *tmp = *head;
- unsigned int lngth = 0;
+	list_t *n;
+	list_t *tmp = *head;
+	unsigned int lngth = 0;
 
- while (str[lngth])
- lngth++;
+	while (str[lngth])
+		lngth++;
 
- n = malloc(sizeof(list_t));
- if (!n)
- return (NULL);
+	n = malloc(sizeof(list_t));
+	if (!n)
+		return (NULL);
 
- n->str = strdup(str);
- n->lngth = lngth;
- n->next = NULL;
+	n->str = strdup(str);
+	n->len = lngth;
+	n->next = NULL;
 
- if (*head == NULL)
- {
- *head = n;
- return (n);
- }
+	if (*head == NULL)
+	{
+		*head = n;
+		return (n);
+ 	}
 
- while (tmp->n)
- temp = tmp->n;
+ 	while (tmp->next)
+ 		tmp = tmp->next;
 
- tmp->next = n;
+ 		tmp->next = n;
 
- return (n);
+ 	return (n);
 }
