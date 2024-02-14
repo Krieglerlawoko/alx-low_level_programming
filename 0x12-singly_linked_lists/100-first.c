6efcee8cc4e655c9afb5362fc6e85e8261a4d95c
Krieglerlawoko <krieglerlawokojudge@gmail.com>
2024-02-14 10:35:28 +0000

    n

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -7,10 +7,8 @@ void first(void) __attribute__ ((constructor));
  * function is executed
  */
 void myStartupFun(void) __attribute__((constructor));
-
 void myStartupFun(void)
 {
    printf("You're beat! and yet, you must allow,\n");
    printf("I bore my house upon my back!\n");
 }
-
